1640417d1fc8a7ccec2f3882272d5b6c12dcb194
Ade Jabar <adejabar@Ades-MacBook-Pro.local>
2019-10-09 18:21:10 +0300

    route-setup

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  { path: '**', component: HomeComponent},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
